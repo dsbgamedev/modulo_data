fa515201062550d38006f1c97674932d0b62f653
Douglas <Douglas@Douglas>
2022-05-02 12:57:15 -0300

    Modulo 15 Aula 5

---
+++ modulo_data/src/cursojava_data/DataEmJava.java
@@ -0,0 +1,34 @@
+package cursojava_data;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DataEmJava {
+	
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date dataVencimentoBoleto = simpleDateFormat.parse("03/05/2022");
+		Date dataAtualHoje =  simpleDateFormat.parse("04/05/2022");
+		
+		/*if(dataVencimentoBoleto.after(dataAtualHoje)) {//Compara se a data é posterior ou maior que a data atual
+			
+			System.out.println("Boleto nao vencido");
+			
+		}else {
+			System.out.println("Boleto vencido - URGENTE");
+		}*/
+		
+		
+		if(dataVencimentoBoleto.before(dataAtualHoje)) {//Se a data um e menor que a data 2
+			
+			System.out.println(" Boleto vencido - URGENTE");
+			
+		}else {
+			System.out.println("Boleto nao vencido");
+		}
+	}
+
+}
