b4b78d0718edf0df3d19a314c4f78809d6708829
Douglas <Douglas@Douglas>
2022-05-02 13:14:49 -0300

    MODULO 15 - AULA 5

--- modulo_data/src/cursojava_data/DataEmJava.java
+++ modulo_data/src/cursojava_data/DataEmJava.java
@@ -3,21 +3,39 @@ package cursojava_data;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DataEmJava {
 	
 	public static void main(String[] args) throws ParseException {
 		
+		/**********************************************************************************/
+		/*Modulo 15 - Aula 6 - Faixa de tempo com ChronoUnit*/
 		
 		
 		
 		
+		/***********************************************************************************/
+		/*Modulo 15 - Aula 5 - Calculando dia, ano e mes com Calendar */
 		
+		/*
+		Calendar calendar = Calendar.getInstance();/*essa instancia pega a data atual*/
+		
+		//Simular a data que vem do banco de dados
+		/*
+		calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("02-05-2022"));/*Definindo uma data qualquer*/
+		
+		/*calendar.add(Calendar.DAY_OF_MONTH, -5);//Somando o dia
+		
+		System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+		*/
+		
+		/***************************************************************************/
 		/*Modulo 15 - Aula 4 - comparando datas usando After e Before */
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		Date dataVencimentoBoleto = simpleDateFormat.parse("03/05/2022");
-		Date dataAtualHoje =  simpleDateFormat.parse("04/05/2022");
+		//SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		//Date dataVencimentoBoleto = simpleDateFormat.parse("03/05/2022");
+		//Date dataAtualHoje =  simpleDateFormat.parse("04/05/2022");
 		
 		/*if(dataVencimentoBoleto.after(dataAtualHoje)) {//Compara se a data é posterior ou maior que a data atual
 			
@@ -28,13 +46,13 @@ public class DataEmJava {
 		}*/
 		
 		
-		if(dataVencimentoBoleto.before(dataAtualHoje)) {//Se a data um e menor que a data 2
+		/*if(dataVencimentoBoleto.before(dataAtualHoje)) {//Se a data um e menor que a data 2
 			
 			System.out.println(" Boleto vencido - URGENTE");
 			
 		}else {
 			System.out.println("Boleto nao vencido");
-		}
+		}*/
 	}
 
 }
